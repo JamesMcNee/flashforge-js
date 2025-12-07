35b93512defda54fd7f3df898b5641079cd18d9f
James McNee <james@jamesmcnee.co.uk>
2025-12-07 13:38:06 +0000

    Add NestJS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello World");
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "src/modules/app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+const port = process.env.FLASHFORGE_PORT || 8080;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Flashforge API")
+    .setDescription("API documentation for FlashforgeJS server")
+    .setVersion("1.0")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
+  app.getHttpAdapter().get("/", (_, res) => {
+    res.redirect("/api");
+  });
+
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}`);
+}
+
+bootstrap();
